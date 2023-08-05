a76c16077f21b56a769207a9188acb751c78b3da
Syed F Ahmad <furquan@protonmail.com>
2023-08-06 01:09:09 +0530

    Fixed the useState type to a generic using null as the default value and added the svg circles to the a full sized svg container mapped from clicks x/y

--- react-circles/src/App.tsx
+++ react-circles/src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { useState } from "react";
 
 function App() {
-  const [clicks, setClicks] = useState([{}]);
+  const [clicks, setClicks] = useState<{ x: number, y: number }[] | null>(null);
   console.log(clicks);
 
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    setClicks(Object.keys(clicks[0]).length === 0 ?
+    setClicks(clicks === null ?
       [{ x: e.clientX, y: e.clientY }] : [...clicks, { x: e.clientX, y: e.clientY }]);
   };
 
   return (
     <main className='h-screen bg-red-50' onClick={handleClick}>
+      <svg height='100%' width='100%'>
+        {clicks && clicks.map((c, i) =>
+          <circle cx={c.x} cy={c.y} r='40' stroke='black' strokeWidth='3' fill='red' key={i + clicks.length} />
+        )}
+      </svg>
     </main>
   );
 }
