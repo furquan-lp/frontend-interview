8f35c17bc0f18cbba8472ff79d78b20cbcd989ff
Syed F Ahmad <furquan@protonmail.com>
2023-08-06 01:36:31 +0530

    Added a reset button at the top of the page inside a header with a handler to clear the circles and movied the clicking area inside a div

--- react-circles/src/App.tsx
+++ react-circles/src/App.tsx
@@ -10,13 +10,23 @@ function App() {
       [{ x: e.clientX, y: e.clientY }] : [...clicks, { x: e.clientX, y: e.clientY }]);
   };
 
+  const handleReset = () => {
+    setClicks(null);
+    console.log('cleared')
+  };
+
   return (
-    <main className='h-screen bg-red-50' onClick={handleClick}>
-      <svg height='100%' width='100%'>
-        {clicks && clicks.map((c, i) =>
-          <circle cx={c.x} cy={c.y} r='40' stroke='black' strokeWidth='3' fill='red' key={i + clicks.length} />
-        )}
-      </svg>
+    <main>
+      <header>
+        <button className='p-1 bg-gray-100 active:bg-gray-400 hover:bg-gray-200' onClick={handleReset}>Reset</button>
+      </header>
+      <div className='h-screen bg-red-50' onClick={handleClick}>
+        <svg height='100%' width='100%'>
+          {clicks && clicks.map((c, i) =>
+            <circle cx={c.x} cy={c.y} r='40' stroke='black' strokeWidth='3' fill='red' key={i + clicks.length} />
+          )}
+        </svg>
+      </div>
     </main>
   );
 }
