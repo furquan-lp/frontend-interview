1d06f345e26e090ee4e03fbbb01c278d7bd6e7f5
Syed F Ahmad <furquan@protonmail.com>
2023-08-06 00:45:50 +0530

    Added basic state for storing click x and y positions using a MouseEvent handler for the App main

--- react-circles/src/App.tsx
+++ react-circles/src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [clicks, setClicks] = useState([{}]);
+  console.log(clicks);
+
+  const handleClick = (e: MouseEvent) => {
+    setClicks(Object.keys(clicks[0]).length === 0 ?
+      [{ x: e.clientX, y: e.clientY }] : [...clicks, { x: e.clientX, y: e.clientY }]);
+  };
+
   return (
-    <main className='h-screen'>
+    <main className='h-screen bg-red-50' onClick={handleClick}>
     </main>
   );
 }
