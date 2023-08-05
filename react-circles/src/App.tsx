778be9705a8daf852180aa27786fa039be7fdc7f
Syed F Ahmad <furquan@protonmail.com>
2023-08-06 00:47:12 +0530

    Fixed type warning for the event handler by using React.MouseEvent<HTMLElement> instead of MouseEvent

--- react-circles/src/App.tsx
+++ react-circles/src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { useState } from "react";
 
 function App() {
   const [clicks, setClicks] = useState([{}]);
   console.log(clicks);
 
-  const handleClick = (e: MouseEvent) => {
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     setClicks(Object.keys(clicks[0]).length === 0 ?
       [{ x: e.clientX, y: e.clientY }] : [...clicks, { x: e.clientX, y: e.clientY }]);
   };
