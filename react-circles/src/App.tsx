d41545c3297915c6404d20fd554dd1ff4bc5ace5
Syed F Ahmad <furquan@protonmail.com>
2023-08-06 01:45:08 +0530

    Added the handler for the undo button that works by slicing clicks and adding the last value to an undoBuffer

--- react-circles/src/App.tsx
+++ react-circles/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
   const [clicks, setClicks] = useState<{ x: number, y: number }[] | null>(null);
-  console.log(clicks);
+  const [undoBuffer, setUndoBuffer] = useState<{ x: number, y: number }[] | null>(null);
+  console.log(undoBuffer);
 
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     setClicks(clicks === null ?
       [{ x: e.clientX, y: e.clientY }] : [...clicks, { x: e.clientX, y: e.clientY }]);
   };
 
+  const handleUndo = (e: React.MouseEvent<HTMLElement>) => {
+    if (clicks !== null) {
+      setUndoBuffer(undoBuffer === null ? [clicks[clicks.length - 1]] : [...undoBuffer, clicks[clicks.length - 1]]);
+      setClicks(clicks.slice(0, clicks.length - 1));
+    }
+  };
+
   const handleReset = () => {
     setClicks(null);
-    console.log('cleared')
   };
 
   return (
@@ -21,7 +28,9 @@ function App() {
         <button className='m-1 mx-2 p-1 bg-gray-100 active:bg-gray-400 hover:bg-gray-200' onClick={handleReset}>
           Reset
         </button>
-        <button className='m-1 mx-2 p-1 bg-gray-100 active:bg-gray-400 hover:bg-gray-200'>Undo</button>
+        <button className='m-1 mx-2 p-1 bg-gray-100 active:bg-gray-400 hover:bg-gray-200' onClick={handleUndo}>
+          Undo
+        </button>
       </header>
       <div className='h-screen bg-red-50' onClick={handleClick}>
         <svg height='100%' width='100%'>
