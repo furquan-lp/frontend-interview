7a9b5959d87f71f3408aa31941c71900a9bb4787
Syed F Ahmad <furquan@protonmail.com>
2023-08-11 18:27:14 +0530

    Added Header to the main container

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <main className='text-center font-bold'>
-      Blank
+    <main>
+      <Header />
     </main>
   );
 }
