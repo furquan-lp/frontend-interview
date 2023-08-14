5d80348e26782f25608c4a25b27239bf7e9bf869
Syed F Ahmad <furquan@protonmail.com>
2023-08-15 00:22:51 +0530

    Changed the colors to slate and added a hover background with transition

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className='flex p-2 bg-white/30 text-2xl justify-between items-center'>
       <span className='font-titlefont text-white md:text-4xl select-none'>frontend-interview 2023</span>
-      <a href="https://github.com/furquan-lp/frontend-interview"
-        className='text-green-50 border border-green-100 p-1' ><AiFillGithub /></a>
+      <a href="https://github.com/furquan-lp/frontend-interview" className='text-slate-50 border border-slate-100
+       p-1 rounded  hover:bg-slate-300/75 hover:border-slate-300 transition-all duration-200' ><AiFillGithub /></a>
     </header>
   );
 }
