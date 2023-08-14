8b84c30a1dfccf1b314a92ecfc2f323c5de66213
Syed F Ahmad <furquan@protonmail.com>
2023-08-15 00:27:44 +0530

    Increase transparency on the header background

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='flex p-2 bg-white/30 text-2xl justify-between items-center'>
       <span className='font-titlefont text-white md:text-4xl select-none'>frontend-interview 2023</span>
       <a href="https://github.com/furquan-lp/frontend-interview" className='text-slate-50 border border-slate-100
-       p-1 rounded  hover:bg-slate-300/75 hover:border-slate-300 transition-all duration-200' ><AiFillGithub /></a>
+       p-1 rounded  hover:bg-slate-300/50 hover:border-slate-300 transition-all duration-200' ><AiFillGithub /></a>
     </header>
   );
 }
