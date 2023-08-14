fec11c809ed1ec4e20af798a94c2c06fd3ea3941
Syed F Ahmad <furquan@protonmail.com>
2023-08-15 00:18:18 +0530

    Replaced the 'Code' link text with a GitHub icon from react-icons and added some styling with a border to it

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,8 +1,11 @@
+import { AiFillGithub } from 'react-icons/ai';
+
 export default function Header() {
   return (
     <header className='flex p-2 bg-white/30 text-2xl justify-between items-center'>
       <span className='font-titlefont text-white md:text-4xl select-none'>frontend-interview 2023</span>
-      <a href="https://github.com/furquan-lp/frontend-interview">Code</a>
+      <a href="https://github.com/furquan-lp/frontend-interview"
+        className='text-green-50 border border-green-100 p-1' ><AiFillGithub /></a>
     </header>
   );
 }
