c7e19eb7ea508925e539bf01e72f9df8167b81bd
Syed F Ahmad <furquan@protonmail.com>
2023-08-15 00:06:50 +0530

    Added a link to the source code and flex with space-between and center items to the header

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
-    <header className='p-2 bg-white/30 text-2xl'>
+    <header className='flex p-2 bg-white/30 text-2xl justify-between items-center'>
       <span className='font-titlefont text-white md:text-4xl'>frontend-interview 2023</span>
+      <a href="https://github.com/furquan-lp/frontend-interview">Code</a>
     </header>
   );
 }
