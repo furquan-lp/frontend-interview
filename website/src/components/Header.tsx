f2e03925ffec7ac9d3ad6c9e11ae224f09e4a46a
Syed F Ahmad <furquan@protonmail.com>
2023-08-15 00:07:25 +0530

    Disabled user select on the title text

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className='flex p-2 bg-white/30 text-2xl justify-between items-center'>
-      <span className='font-titlefont text-white md:text-4xl'>frontend-interview 2023</span>
+      <span className='font-titlefont text-white md:text-4xl select-none'>frontend-interview 2023</span>
       <a href="https://github.com/furquan-lp/frontend-interview">Code</a>
     </header>
   );
