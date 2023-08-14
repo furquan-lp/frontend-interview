b8fec7807a9ebdd2ee8813df97d211e8ce2cd5ea
Syed F Ahmad <furquan@protonmail.com>
2023-08-14 23:58:32 +0530

    Added the title text with titlefont and increased transparency on the header

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className='p-2 bg-white/50 text-2xl'>
-      Blank
+    <header className='p-2 bg-white/30 text-2xl'>
+      <span className='font-titlefont text-white md:text-4xl'>frontend-interview 2023</span>
     </header>
   );
 }
