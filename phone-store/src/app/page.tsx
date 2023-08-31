b20c65674e72f04d73e8ba39d84500d799776af0
Syed F Ahmad <furquan@protonmail.com>
2023-08-31 23:55:01 +0530

    Added Header and a ProductCard (for testing) to the page

--- phone-store/src/app/page.tsx
+++ phone-store/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image';
 
+import { ProductCard } from './components/product';
+import Header from './components/header';
+
 export default function Home() {
   return (
     <main>
-      Blank App
+      <Header />
+      <section className='flex flex-col items-center'>
+        <ProductCard name='Hello Phone 1' price={999} />
+      </section>
     </main>
   );
 }
