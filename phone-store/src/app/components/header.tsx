10912c83a5552ec25f03edce6e21204179f6c310
Syed F Ahmad <furquan@protonmail.com>
2023-08-31 23:53:26 +0530

    Added a select dropdown and some more basic styling

--- phone-store/src/app/components/header.tsx
+++ phone-store/src/app/components/header.tsx
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
-    <header className='flex items-center p-2 bg-cyan-600 justify-between'>
+    <header className='flex items-center p-2 bg-cyan-600 justify-between m-2 rounded-lg'>
       <span className='font-source-code-pro text-2xl text-white'>phone_store</span>
-      <input type='text' placeholder='Enter Search Values' className='p-1 bg-slate-100'></input>
+      <input type='text' placeholder='Enter Search Values' className='p-1 text-lg w-1/2 rounded-lg my-0.5' />
+      <select name='filter' id='filter' className='p-1 rounded-md'>
+        <option value='placeholder'>Placeholder for filter</option>
+      </select>
     </header>
   );
 }
