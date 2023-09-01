045972694e2cc1bedcd03fd8e32e39ac5d25cd17
Syed F Ahmad <furquan@protonmail.com>
2023-09-01 23:40:30 +0530

    Added placeholder selects for brand filter and price filter instead of the old placeholder for filter

--- phone-store/src/app/components/header.tsx
+++ phone-store/src/app/components/header.tsx
@@ -1,11 +1,16 @@
 export default function Header() {
   return (
-    <header className='flex items-center p-2 bg-cyan-600 justify-between m-2 rounded-lg'>
-      <span className='font-source-code-pro text-2xl text-white'>phone_store</span>
+    <header className='flex items-center p-2 bg-cyan-800 justify-between m-2 rounded-lg'>
+      <span className='font-source-code-pro italic text-2xl text-white select-none'>phone_store</span>
       <input type='text' placeholder='Enter Search Values' className='p-1 text-lg w-1/2 rounded-lg my-0.5' />
-      <select name='filter' id='filter' className='p-1 rounded-md'>
-        <option value='placeholder'>Placeholder for filter</option>
-      </select>
+      <span>
+        <select name='brand-filter' id='brand-filter' className='p-1 rounded-md'>
+          <option value='placeholder'>Brand filter</option>
+        </select>
+        <select name='price-filter' id='price-filter' className='p-1 rounded-md mx-2'>
+          <option value='placeholder'>Price filter</option>
+        </select>
+      </span>
     </header>
   );
 }
