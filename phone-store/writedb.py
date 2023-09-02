a8c7772e40154b75e0713b4d33d557596bcc317c
Syed F Ahmad <furquan@protonmail.com>
2023-09-02 22:17:10 +0530

    Added a method to generate random phone names to later write to the PostgreSQL database

---
+++ phone-store/writedb.py
@@ -0,0 +1,28 @@
+import os
+import random
+import string
+import psycopg2
+
+brands = [ 'Samsung', 'Apple', 'Google', 'OnePlus', 'Asus', 'Nokia', 'Xiaomi', 'Vivo', 'Oppo' ]
+words = [ 'Call', 'Sky', 'Jet', 'Lumen', 'Bold', 'Glass', 'Gold' ]
+superlatives = [ 'Pro', 'Plus', 'Pro+', 'Super', 'Ultra' ]
+
+def generate_phone_name():
+    brand_name = random.choice(brands)
+    word = random.choice(words)    
+    first_letter = random.choice(string.ascii_uppercase)
+    second_letter = random.choice([*list(string.ascii_uppercase), *['']])
+    number = random.choice(string.digits.replace('0', ''))
+    extra = random.choice(superlatives) if random.random() < 0.2 else ''
+    
+    if brand_name == 'Samsung':
+        word = random.choice([ 'Galaxy', 'Note', *words])
+    elif brand_name == 'Google':
+        word = 'Pixel'
+    elif brand_name == 'Apple':
+        word = 'iPhone'
+        first_letter = random.choice(['s', 'c', 'SE', 'XS', 'X']) if random.random() < 0.2 else ''
+        extra = random.choice(superlatives) if random.random() < 0.5 else ''
+        return f'{brand_name} {word} {number}{first_letter} {extra}'
+
+    return f'{brand_name} {word} {first_letter}{second_letter}{number} {extra}'
