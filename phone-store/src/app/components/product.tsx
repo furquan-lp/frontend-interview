4edb4d1168ce701e2e1d3db80688fbd69f4a1c2c
Syed F Ahmad <furquan@protonmail.com>
2023-09-02 15:05:19 +0530

    Replaced mx with gap-x

--- phone-store/src/app/components/product.tsx
+++ phone-store/src/app/components/product.tsx
@@ -20,10 +20,10 @@ export function ProductCard({ imageUrl, name, brand, price }: ProductPhone) {
 
 export function ProductInfoDialog({ imageUrl, name, brand, price, open }: ProductPhone & { open: boolean }) {
   return (
-    <dialog open={open} className='flex p-2 border'>
+    <dialog open={open} className='flex gap-x-10 p-2 border shadow rounded'>
       <Image src={`/${imageUrl || 'sample-phone.webp'}`} width={320} height={480} alt={`product ${name} photo`}
         className='m-2 mx-10' />
-      <section className='flex flex-col mx-10 mt-10 justify-around'>
+      <section className='flex flex-col mt-10 justify-around'>
         <span>
           <span className='md:text-4xl m-1'>{name}</span>
           <span className='flex items-center m-1 my-4 text-xl'>Rating: {[...Array(5)].map((e, i) =>
