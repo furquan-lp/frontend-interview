70104ad74b60939e865800cd4e78fdc1cf64ea6b
Syed F Ahmad <furquan@protonmail.com>
2023-09-11 23:11:20 +0530

    Changed the GET function so it returns a JSON Map with PhoneObject[] entries for every brand (which are fetched from the phone_metadata table)

--- phone-store/src/app/api/db/route.ts
+++ phone-store/src/app/api/db/route.ts
@@ -3,9 +3,14 @@ import conn from '@/app/lib/db';
 
 export async function GET() {
   try {
-    const query: string = 'SELECT * FROM phone_models;'
-    const result = await conn.query(query);
-    return NextResponse.json(result.rows);
+    const metadata = await conn.query('SELECT * FROM phone_metadata;');
+    const brandsList: string[] = metadata.rows[0].brands;
+    const map1 = new Map();
+    for (const brand of brandsList) {
+      const brandQuery = await conn.query(`SELECT * FROM phone_models WHERE brand = '${brand}';`);
+      map1.set(brand, brandQuery.rows);
+    }
+    return NextResponse.json(Object.fromEntries(map1));
   } catch (error) {
     console.error('DB Error encountered.', error);
   }
