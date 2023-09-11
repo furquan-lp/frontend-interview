b516d3fb3e6ac89f4d71048cd8e891b69d9e4436
Syed F Ahmad <furquan@protonmail.com>
2023-09-11 18:23:11 +0530

    Made the page client side and added basic state and effect to fetch and store the data from the backend into an array of PhoneObjects and then displayed them using ProductCards

--- phone-store/src/app/page.tsx
+++ phone-store/src/app/page.tsx
@@ -1,15 +1,47 @@
-import Image from 'next/image';
-
-import { ProductCard } from './components/product';
+'use client';
+import { useRef, useState, useEffect } from 'react';
+import { ProductCard, ProductInfoDialog } from './components/product';
 import Header from './components/header';
 
+interface PhoneObject {
+  id: string,
+  brand: string,
+  model: string,
+  price: number,
+  image: string,
+}
+
 export default function Home() {
+  const [phones, setPhones] = useState<PhoneObject[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch('http://localhost:3000/api/db');
+      let tx = await data.json();
+      setPhones(tx);
+    })();
+  }, []);
+
   return (
     <main>
       <Header />
-      <section className='flex flex-col items-center'>
-        <ProductCard name='Hello Phone 1' price={999} />
+      <section className='flex items-center gap-2 flex-wrap mx-2'>
+        {phones.length && phones.map((p: PhoneObject) => <ProductCard name={p.model} brand={p.brand} price={p.price}
+          imageUrl={`phones/${p.image}`} key={p.id} />)}
       </section>
     </main>
   );
 }
+
+/* export default async function Home() {
+  let data = await fetch('http://localhost:3000/api/db');
+  let tx = await data.json();
+  return (
+    <main>
+      <Header />
+      <section className='flex items-center gap-2 flex-wrap mx-2'>
+        {tx.map((t: PhoneObject) => <ProductCard name={t.model} brand={t.brand} price={t.price} imageUrl={`phones/${t.image}`} key={t.id} />)}
+      </section>
+    </main>
+  );
+} */
