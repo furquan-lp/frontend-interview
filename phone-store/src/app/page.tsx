4a3efd2c87788e0e17b7431e101cc0adb73351c7
Syed F Ahmad <furquan@protonmail.com>
2023-09-16 19:48:05 +0530

    Added modified binary search for the price filtering, cached using a PriceIndex custom type

--- phone-store/src/app/page.tsx
+++ phone-store/src/app/page.tsx
@@ -11,10 +11,16 @@ interface PhoneObject {
   image: string,
 }
 
+interface PriceIndex {
+  a: number, b: number;
+}
+
 export default function Home() {
   const [phoneMap, setPhoneMap] = useState<Map<string, PhoneObject[]>>(new Map());
-  const [phones, setPhones] = useState<PhoneObject[]>([]);
+  const [phones, setPhones] = useState<[PhoneObject[], PhoneObject[]]>([[], []]);
   const [brandfilter, setBrandfilter] = useState<string>('');
+  const [pricefilter, setPriceFilter] = useState<string>('');
+  const priceIndices = useRef<PriceIndex[]>([]);
   const [phoneMetadata, setPhoneMetadata] = useState<{ brands: string[] }>({ brands: [] });
 
   useEffect(() => {
@@ -23,13 +29,12 @@ export default function Home() {
       let tx = await data.json();
       setPhoneMap(new Map(Object.entries(tx)));
       setBrandfilter('none');
+      setPriceFilter('none');
       let metadataBrands = await fetch('http://localhost:3000/api/metadata/brands');
       let metadata = await metadataBrands.json();
       setPhoneMetadata({ brands: metadata });
     })();
   }, []);
-
-  console.log(phoneMetadata)
 
   useEffect(() => {
     let arr: PhoneObject[] = [];
@@ -39,14 +44,62 @@ export default function Home() {
       }
     });
     arr.sort((a, b) => a.price < b.price ? 0 : 1);
-    setPhones(arr);
+    setPhones([arr, arr]);
   }, [brandfilter]);
+
+  useEffect(() => {
+    if (pricefilter !== 'none' && pricefilter.length) {
+      let filterStr: string[] = pricefilter.split(',');
+
+      const closestSearch = function (arr: PhoneObject[], n: number): number {
+        console.log('finding', n);
+        let value = n * 1000;
+        if (value < arr[0].price) {
+          return 0;
+        }
+        if (value > arr[arr.length - 1].price) {
+          return arr.length - 1;
+        }
+
+        let lo = 0;
+        let hi = arr.length - 1;
+        while (lo <= hi) {
+          let mid = Math.floor((hi + lo) / 2);
+
+          if (value < arr[mid].price) {
+            hi = mid - 1;
+          } else if (value > arr[mid].price) {
+            lo = mid + 1;
+          } else {
+            return mid;
+          }
+        }
+
+        return (arr[lo].price - value) < (value - arr[hi].price) ? lo : hi;
+      }
+
+      if (priceIndices.current === null || priceIndices.current.length === 0) {
+        priceIndices.current = new Array<PriceIndex>(Number(filterStr[3]));
+      }
+
+      priceIndices.current[Number(filterStr[0])] = {
+        a: closestSearch(phones[1], Number(filterStr[1])),
+        b: closestSearch(phones[1], Number(filterStr[2]))
+      };
+
+      console.log('current indices', priceIndices.current);
+      setPhones([phones[1].slice(priceIndices.current[Number(filterStr[0])].a, priceIndices.current[Number(filterStr[0])].b), phones[1]]);
+    } else if (pricefilter === 'none' && phones[1].length) {
+      console.log('setting to')
+      setPhones([phones[1], phones[1]]);
+    }
+  }, [pricefilter]);
 
   return (
     <main>
-      <Header setBrand={setBrandfilter} brands={phoneMetadata.brands} />
+      <Header setBrand={setBrandfilter} brands={phoneMetadata.brands} setPrice={setPriceFilter} />
       <section className='flex items-center gap-2 flex-wrap mx-2'>
-        {phones.length && phones.map((p: PhoneObject) => <ProductCard name={p.model} brand={p.brand} price={p.price}
+        {phones[0].length && phones[0].map((p: PhoneObject) => <ProductCard name={p.model} brand={p.brand} price={p.price}
           imageUrl={`phones/${p.image}`} key={p.id} />)}
       </section>
     </main>
