fc41d8a1d8898a5af1d37b39125a164d203901de
Syed F Ahmad <furquan@protonmail.com>
2023-09-21 15:44:11 +0530

    Added a fetched boolean prop to disable the search and the selects when the data is being fetched

--- phone-store/src/app/components/header.tsx
+++ phone-store/src/app/components/header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import { MutableRefObject } from 'react';
 
-export default function Header({ setBrand, brands, setPrice, searchValue, setSearch }: {
-  setBrand: Function,
-  brands: string[], setPrice: Function, searchValue: MutableRefObject<string>, setSearch: Function
+export default function Header({ setBrand, brands, setPrice, searchValue, setSearch, fetched }: {
+  setBrand: Function, brands: string[], setPrice: Function, searchValue: MutableRefObject<string>, setSearch: Function,
+  fetched: boolean
 }) {
   return (
     <header className='flex items-center p-2 bg-cyan-800 justify-between m-2 rounded-lg'>
@@ -14,13 +14,14 @@ export default function Header({ setBrand, brands, setPrice, searchValue, setSea
             if (e.key === 'Enter') {
               setSearch(searchValue.current);
             }
-          }} />
-        <select onChange={(e) => setBrand(e.target.value)}
-          className='p-1 px-2 rounded-md bg-white'>
+          }} disabled={!fetched} />
+        <select onChange={(e) => setBrand(e.target.value)} className='p-1 px-2 rounded-md bg-white'
+          disabled={!fetched}>
           <option value='none'>Select Brand</option>
           {brands.length && brands.map((b, i) => <option value={b} key={b + i}>{b}</option>)}
         </select>
-        <select onChange={(e) => setPrice(e.target.value)} className='p-1 px-2 rounded-md bg-white' >
+        <select onChange={(e) => setPrice(e.target.value)} className='p-1 px-2 rounded-md bg-white'
+          disabled={!fetched}>
           <option value='none'>Select Price</option>
           <option value='0,6,10,4'>&#8377;6000 to &#8377;10,000</option>
           <option value='1,10,20,4'>&#8377;10,000 to &#8377;20,000</option>
