36136de7edf05a73f6cc3759bb4965d150da37c7
Syed F Ahmad <furquan@protonmail.com>
2023-09-23 23:59:11 +0530

    Added dark mode background

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -4,7 +4,7 @@ import SQLField from './components/sqlfield';
 
 export default function Home() {
   return (
-    <main className='flex flex-col'>
+    <main className='flex flex-col dark:bg-slate-700'>
       <Header />
       <article className='flex gap-x-2 m-2'>
         <SQLField />
