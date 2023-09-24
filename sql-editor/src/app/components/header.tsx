906a74109f598bd97df2d73b8bdc77cba6f7d944
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 21:34:32 +0530

    Added styling for the buttons (hover, pointer, etc.)

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -3,9 +3,10 @@ export default function Header({ version }: { version?: number }) {
     <header className='flex items-center justify-between bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
       <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
       <span className='flex gap-1 items-center'>
-        <span className='material-symbols-outlined'>dark_mode</span>
+        <span className='material-symbols-outlined cursor-pointer select-none hover:bg-emerald-600
+         dark:hover:bg-sky-600 p-1 rounded-full'>dark_mode</span>
         <a href='https://github.com/furquan-lp/frontend-interview/tree/master/sql-editor'
-          className='material-symbols-outlined border p-0.5 rounded-md'>code</a>
+          className='material-symbols-outlined border p-0.5 rounded-md select-none'>code</a>
       </span>
     </header>
   );
