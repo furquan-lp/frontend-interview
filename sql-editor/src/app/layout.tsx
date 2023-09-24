f5be5a9eacede61a893736664c208441e22dd4cb
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 13:19:26 +0530

    Added material design icons stylesheet from Google Fonts

--- sql-editor/src/app/layout.tsx
+++ sql-editor/src/app/layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet" />
       <body>{children}</body>
     </html>
   )
