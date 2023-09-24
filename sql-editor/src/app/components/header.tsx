a5cd309e9babbf72e7294c097f4308bdab463662
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 21:59:12 +0530

    Added Run and Clear buttons with all the necessary styling including dark mode colors

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -1,7 +1,15 @@
 export default function Header({ version }: { version?: number }) {
   return (
     <header className='flex items-center justify-between bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
-      <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
+      <span className='flex gap-1 items-center'>
+        <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
+        <button className='rounded bg-green-600 active:bg-green-500 dark:bg-cyan-600 dark:active:bg-cyan-500
+         text-lg font-bold ml-6 px-2 p-1'>Run <span
+            className='material-symbols-outlined align-[-6px]'>arrow_forward</span></button>
+        <button className='rounded bg-emerald-600 active:bg-emerald-500 dark:bg-sky-600 dark:active:bg-sky-500
+         text-lg font-bold px-2 p-1'>Clear <span
+            className='material-symbols-outlined align-[-6px]'>clear_all</span></button>
+      </span>
       <span className='flex gap-1 items-center'>
         <span className='material-symbols-outlined cursor-pointer select-none hover:bg-emerald-600
          dark:hover:bg-sky-600 p-1 rounded-full'>dark_mode</span>
