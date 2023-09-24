a5cad3a6a04f20fff02afc9a3659450140f10c2d
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 22:08:12 +0530

    Hide the Run and Clear text on the buttons on screens smaller than md

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -4,10 +4,10 @@ export default function Header({ version }: { version?: number }) {
       <span className='flex gap-1 items-center'>
         <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
         <button className='rounded bg-green-600 active:bg-green-500 dark:bg-cyan-600 dark:active:bg-cyan-500
-         text-lg font-bold ml-6 px-2 p-1'>Run <span
+         text-lg font-bold ml-6 px-2 p-1'><span className='hidden md:inline'>Run</span> <span
             className='material-symbols-outlined align-[-6px]'>arrow_forward</span></button>
         <button className='rounded bg-emerald-600 active:bg-emerald-500 dark:bg-sky-600 dark:active:bg-sky-500
-         text-lg font-bold px-2 p-1'>Clear <span
+         text-lg font-bold px-2 p-1'><span className='hidden md:inline'>Clear</span> <span
             className='material-symbols-outlined align-[-6px]'>clear_all</span></button>
       </span>
       <span className='flex gap-1 items-center'>
