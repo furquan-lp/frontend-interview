2f99b1e8bd8b1f7dc2b152e7d9d2f501274c3896
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 21:03:15 +0530

    Added an optional text prop to show in the textarea

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -1,7 +1,7 @@
-export default function SQLOutputField() {
+export default function SQLOutputField({ text }: { text?: string }) {
   return (
     <textarea className='border border-slate-300 bg-slate-500 text-white dark:bg-slate-50
      dark:border-slate-900 dark:text-slate-700 w-full h-96 p-2 py-5 rounded-md shadow focus:outline-none resize-none
-      font-mono md:text-lg' value="SQLite v1.0" readOnly />
+      font-mono md:text-lg' value={`${text ? text : 'Loading...'}`} readOnly />
   );
 }
