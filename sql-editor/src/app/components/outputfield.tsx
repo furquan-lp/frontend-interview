46a45d0cfc188cb0aa36eed6d1128d38e887af7f
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 20:52:04 +0530

    Added dark mode colors, set font to monospaced and increased font size

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -1,7 +1,7 @@
 export default function SQLOutputField() {
   return (
-    <textarea className='border border-slate-300 bg-slate-500 text-white w-full h-96 p-2 py-5 rounded-md shadow
-     focus:outline-none resize-none' value="SQLite v1.0" readOnly>
-    </textarea>
+    <textarea className='border border-slate-300 bg-slate-500 text-white dark:bg-slate-50
+     dark:border-slate-900 dark:text-slate-700 w-full h-96 p-2 py-5 rounded-md shadow focus:outline-none resize-none
+      font-mono md:text-lg' value="SQLite v1.0" readOnly />
   );
 }
