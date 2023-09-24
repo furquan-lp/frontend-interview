6b3416ed0e0b300eb1be3c0dcfcd9ed0b108943a
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 21:23:01 +0530

    Added a dark mode and a code icon (with a link to the repository) along with flex styling

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -1,7 +1,12 @@
 export default function Header({ version }: { version?: number }) {
   return (
-    <header className='bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
+    <header className='flex items-center justify-between bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
       <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
+      <span className='flex gap-1 items-center'>
+        <span className='material-symbols-outlined'>dark_mode</span>
+        <a href='https://github.com/furquan-lp/frontend-interview/tree/master/sql-editor'
+          className='material-symbols-outlined border p-0.5 rounded-md'>code</a>
+      </span>
     </header>
   );
 }
