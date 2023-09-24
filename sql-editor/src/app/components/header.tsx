08ccb6fd4793d24b50921994fa32ba5f4bc3a9cf
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 20:55:34 +0530

    Added an optional version number prop instead of a static version string

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -1,7 +1,7 @@
-export default function Header() {
+export default function Header({ version }: { version?: number }) {
   return (
     <header className='bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
-      <span className='text-2xl font-logofont select-none'>SQL Editor 0.1</span>
+      <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
     </header>
   );
 }
