320b0be8dd9d7e4edc81aec4cbe5cb3233e6f123
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 12:54:37 +0530

    Increased padding and added vertical padding

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -1,6 +1,6 @@
 export default function SQLOutputField() {
   return (
-    <textarea className='border border-slate-300 bg-slate-500 text-white w-full h-96 p-1 rounded-md shadow
+    <textarea className='border border-slate-300 bg-slate-500 text-white w-full h-96 p-2 py-5 rounded-md shadow
      focus:outline-none resize-none' value="SQLite v1.0" readOnly>
     </textarea>
   );
