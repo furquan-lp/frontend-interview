99a8f7630ef615154e2aad4fc4204bb941146c3b
Syed F Ahmad <furquan@protonmail.com>
2023-09-24 20:30:13 +0530

    Added dark mode background color

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='bg-emerald-700 p-2 text-white'>
+    <header className='bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
       <span className='text-2xl'>SQL Editor</span>
     </header>
   );
