020259378ea22382f37167f59e39c3c873821c5e
Syed F Ahmad <furquan@protonmail.com>
2023-09-25 18:50:36 +0530

    Render page on the client and pass the setDarkMode state function to Header

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import Header from './components/header';
 import SQLOutputField from './components/outputfield';
@@ -8,15 +9,15 @@ export default function Home() {
 
   useEffect(() => {
     localStorage.setItem('darkmode', 'false');
-  }, [])
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('darkmode', String(darkMode));
-  }, [darkMode])
+  }, [darkMode]);
 
   return (
     <main className='flex flex-col dark:bg-slate-700'>
-      <Header version={0.1} />
+      <Header version={0.1} setDarkMode={setDarkMode} />
       <article className='flex flex-wrap md:flex-nowrap gap-y-2 md:gap-x-2 m-2'>
         <SQLField />
         <SQLOutputField />
