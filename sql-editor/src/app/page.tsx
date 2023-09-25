7ff505155b0dbe58a4d795cff9fe479b6cff0a2c
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 00:39:09 +0530

    Added the sql-loader script to the page

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from './components/header';
 import SQLOutputField from './components/outputfield';
 import SQLField from './components/sqlfield';
 import Footer from './components/footer';
-
+import Script from 'next/script';
 export default function Home() {
   const [darkMode, setDarkMode] = useState('');
 
@@ -23,13 +23,16 @@ export default function Home() {
   }, [darkMode]);
 
   return (
-    <main className='flex flex-col dark:bg-slate-700 min-h-screen'>
-      <Header version={0.3} setDarkMode={setDarkMode} theme={darkMode} />
-      <article className='flex flex-wrap md:flex-nowrap gap-y-2 md:gap-x-2 m-2'>
-        <SQLField />
-        <SQLOutputField />
-      </article>
-      <Footer />
-    </main>
+    <>
+      <Script type="module" strategy='beforeInteractive' src="/sql-loader.js" />
+      <main className='flex flex-col dark:bg-slate-700 min-h-screen'>
+        <Header version={0.3} setDarkMode={setDarkMode} theme={darkMode} />
+        <article className='flex flex-wrap md:flex-nowrap gap-y-2 md:gap-x-2 m-2'>
+          <SQLField />
+          <SQLOutputField />
+        </article>
+        <Footer />
+      </main>
+    </>
   );
 }
