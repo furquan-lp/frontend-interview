5783a67d5af0d98565075af14a59032fb69a163c
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 00:31:19 +0530

    Changed SQLOutputField to a generic container containing span elements (to be added)

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -1,7 +1,7 @@
 export default function SQLOutputField({ text }: { text?: string }) {
   return (
-    <textarea className='border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
+    <div className='border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
      dark:border-slate-900 dark:text-slate-700 w-full h-64 md:h-96 p-2 py-5 rounded-md shadow focus:outline-none
-      resize-none font-mono md:text-lg' value={`${text ? text : 'Loading...'}`} readOnly />
+      resize-none font-mono md:text-lg'><span>{text ? text : 'Loading...'}</span></div>
   );
 }
