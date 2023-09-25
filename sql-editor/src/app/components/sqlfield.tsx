9308a51ba436127a99fc00990d2e87a6f1daf6ce
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 00:57:59 +0530

    Added onChange prop and placeholder text

--- sql-editor/src/app/components/sqlfield.tsx
+++ sql-editor/src/app/components/sqlfield.tsx
@@ -1,7 +1,9 @@
-export default function SQLField() {
+import { ChangeEventHandler, MutableRefObject } from "react";
+
+export default function SQLField({ onChange }: { onChange: ChangeEventHandler<HTMLTextAreaElement> }) {
   return (
     <textarea className='border border-slate-300 bg-slate-50 bg-clip-content w-full h-64 md:h-96 p-2 py-5
      rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600 dark:text-slate-50 font-editorfont
-      md:text-xl'/>
+      md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} />
   );
 }
