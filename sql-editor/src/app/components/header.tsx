31c1fc01c6782846ede60415f99914a86fb60229
Syed F Ahmad <furquan@protonmail.com>
2023-09-25 20:29:23 +0530

    Use string types for the dark mode state and use localStorage.theme instead of a custom key

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -12,8 +12,8 @@ export default function Header({ version, setDarkMode }: { version?: number, set
       </span>
       <span className='flex gap-1 items-center'>
         <button className='material-symbols-outlined hover:bg-emerald-600 dark:hover:bg-sky-600 p-1 rounded-full'
-          onClick={() => setDarkMode(!(localStorage.getItem('darkmode') === 'true'))}>
-          {localStorage.getItem('darkmode') === 'true' ? <>light_mode</> : <>dark_mode</>}</button>
+          onClick={() => setDarkMode(localStorage.theme === 'dark' ? 'light' : 'dark')}>
+          {localStorage.theme === 'light' ? <>light_mode</> : <>dark_mode</>}</button>
         <a href='https://github.com/furquan-lp/frontend-interview/tree/master/sql-editor'
           className='material-symbols-outlined border p-0.5 rounded-md select-none'>code</a>
       </span>
