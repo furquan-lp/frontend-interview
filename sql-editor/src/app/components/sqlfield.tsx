72a27566feb0d101a3c4af6db13168e9ed7722a0
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 02:31:23 +0530

    Adjusted padding so it looks better on mobile

--- sql-editor/src/app/components/sqlfield.tsx
+++ sql-editor/src/app/components/sqlfield.tsx
@@ -5,8 +5,9 @@ export default function SQLField({ onChange, loaded }: {
   loaded: boolean
 }) {
   return (
-    <textarea className='border border-slate-300 bg-slate-50 bg-clip-content w-full h-64 md:h-96 p-2 py-5
-     rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600 dark:text-slate-50 font-editorfont
-      md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} disabled={!loaded} id='sqltextarea' />
+    <textarea className='border border-slate-300 bg-slate-50 bg-clip-content w-full h-64 md:h-96 p-1 py-2 md:p-2
+     md:py-5 rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600 dark:text-slate-50
+      font-editorfont md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} disabled={!loaded}
+      id='sqltextarea' />
   );
 }
