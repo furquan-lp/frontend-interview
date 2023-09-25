ff899a30644f88b53bc091b0a74d7cb562aa7026
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 02:19:15 +0530

    Added a loaded boolean prop to keep the textarea disabled until the database has been loaded and added an id to it

--- sql-editor/src/app/components/sqlfield.tsx
+++ sql-editor/src/app/components/sqlfield.tsx
@@ -1,9 +1,12 @@
 import { ChangeEventHandler, MutableRefObject } from "react";
 
-export default function SQLField({ onChange }: { onChange: ChangeEventHandler<HTMLTextAreaElement> }) {
+export default function SQLField({ onChange, loaded }: {
+  onChange: ChangeEventHandler<HTMLTextAreaElement>,
+  loaded: boolean
+}) {
   return (
     <textarea className='border border-slate-300 bg-slate-50 bg-clip-content w-full h-64 md:h-96 p-2 py-5
      rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600 dark:text-slate-50 font-editorfont
-      md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} />
+      md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} disabled={!loaded} id='sqltextarea' />
   );
 }
