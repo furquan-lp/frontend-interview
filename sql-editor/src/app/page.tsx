1e3a0214818e953da50b735504fcb0354e90db42
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 01:23:16 +0530

    Used the useDB hook to load the database keeping the SQLField disabled until its loaded

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -5,9 +5,13 @@ import SQLOutputField from './components/outputfield';
 import SQLField from './components/sqlfield';
 import Footer from './components/footer';
 import Script from 'next/script';
+import { useDB } from './lib/hooks';
+
 export default function Home() {
   const [darkMode, setDarkMode] = useState('');
   let editorText = useRef('');
+
+  const database: any = useDB();
 
   useEffect(() => {
     if (darkMode === '') {
@@ -17,9 +21,9 @@ export default function Home() {
     }
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) &&
       window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
+      document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove('dark');
     }
   }, [darkMode]);
 
@@ -27,10 +31,10 @@ export default function Home() {
     <>
       <Script type="module" strategy='beforeInteractive' src="/sql-loader.js" />
       <main className='flex flex-col dark:bg-slate-700 min-h-screen'>
-        <Header version={0.3} setDarkMode={setDarkMode} theme={darkMode} />
+        <Header version={0.5} clickRun={() => console.log('run')} setDarkMode={setDarkMode} theme={darkMode} />
         <article className='flex flex-wrap md:flex-nowrap gap-y-2 md:gap-x-2 m-2'>
-          <SQLField onChange={(e) => editorText.current = e.target.value} />
-          <SQLOutputField />
+          <SQLField onChange={(e) => editorText.current = e.target.value} loaded={database !== null} />
+          <SQLOutputField text={database === null ? undefined : 'Loaded SQLite from sql.js v1.8.0.'} />
         </article>
         <Footer />
       </main>
