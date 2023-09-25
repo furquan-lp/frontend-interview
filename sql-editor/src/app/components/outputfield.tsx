5d4442fcd970bad8737ff42dd30ecdeca07b7371
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 01:41:15 +0530

    Replaced the text prop with a strings array that is mapped to span elements (styled red if the text contains 'Error')

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -1,7 +1,11 @@
-export default function SQLOutputField({ text }: { text?: string }) {
+export default function SQLOutputField({ messages }: { messages?: string[] }) {
   return (
-    <div className='border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
+    <div className='flex flex-col border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
      dark:border-slate-900 dark:text-slate-700 w-full h-64 md:h-96 p-2 py-5 rounded-md shadow focus:outline-none
-      resize-none font-mono md:text-lg'><span>{text ? text : 'Loading...'}</span></div>
+      resize-none font-mono md:text-lg'>
+      {messages ? messages.map(m =>
+        <span className={m.includes('Error') ? 'text-red-400 dark:text-orange-500' : undefined}>{m}</span>
+      ) : 'Loading...'}
+    </div>
   );
 }
