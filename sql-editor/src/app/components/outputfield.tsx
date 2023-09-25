e20aa414a353f80b377a68aec5b67116624c4709
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 02:19:38 +0530

    Added a key prop

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -2,10 +2,9 @@ export default function SQLOutputField({ messages }: { messages?: string[] }) {
   return (
     <div className='flex flex-col border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
      dark:border-slate-900 dark:text-slate-700 w-full h-64 md:h-96 p-2 py-5 rounded-md shadow focus:outline-none
-      resize-none font-mono md:text-lg'>
-      {messages ? messages.map(m =>
-        <span className={m.includes('Error') ? 'text-red-400 dark:text-orange-500' : undefined}>{m}</span>
-      ) : 'Loading...'}
+      resize-none font-mono md:text-lg'>{messages ? messages.map((m, i) => <span className={m.includes('Error') ?
+      'text-red-400 dark:text-orange-500' : undefined} key={i + m}>{m}</span>
+    ) : 'Loading...'}
     </div>
   );
 }
