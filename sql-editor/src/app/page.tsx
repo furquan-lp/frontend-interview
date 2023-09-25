55339c9e8bac15f9cdbd3b7ecda36762242494e8
Syed F Ahmad <furquan@protonmail.com>
2023-09-25 18:33:17 +0530

    Added state for dark mode and stored it as a key in local storage

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
 import Header from './components/header';
 import SQLOutputField from './components/outputfield';
 import SQLField from './components/sqlfield';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('darkmode', 'false');
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('darkmode', String(darkMode));
+  }, [darkMode])
+
   return (
     <main className='flex flex-col dark:bg-slate-700'>
       <Header version={0.1} />
