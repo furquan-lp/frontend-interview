c5afa0fc3235b4bb8f6cd50c3ada1aea34075149
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 18:26:41 +0530

    Removed the vertical margin on the SQL fields article and added a gap-y on the main container instead

--- sql-editor/src/app/page.tsx
+++ sql-editor/src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
   return (
     <>
       <Script type="module" strategy='beforeInteractive' src="/sql-loader.js" />
-      <main className='flex flex-col dark:bg-slate-700 min-h-screen'>
+      <main className='flex flex-col gap-y-2 dark:bg-slate-700 min-h-screen'>
         <Header version={0.8} clickRun={() => runQuery(editorText.current)} clickClear={() => {
           let e = document.getElementById('sqltextarea') as HTMLTextAreaElement;
           if (e) {
@@ -89,7 +89,7 @@ export default function Home() {
           }
         }} setDarkMode={setDarkMode}
           theme={darkMode} />
-        <article className='flex flex-wrap md:flex-nowrap gap-y-2 mx-1 my-2 md:gap-x-2 md:m-2'>
+        <article className='flex flex-wrap md:flex-nowrap gap-y-2 mx-1 md:gap-x-2 md:mx-2'>
           <SQLField onChange={(e) => editorText.current = e.target.value} loaded={database !== null} />
           <SQLOutputField messages={database === null ? undefined : messages} />
         </article>
