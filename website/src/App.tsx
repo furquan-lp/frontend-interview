1711eca28d80352c0478d20d8521d788cd29fe4c
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 23:16:14 +0530

    Adjusted some styling in AssignmentCard and added the problem statement for react circles

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -18,7 +18,7 @@ function AssignmentCard({ title, blurb, image, tags, livelink, repolink, childre
         {tags && <div className='flex my-2'>{tags.map(t => <AssignmentCardTag tag={t} />)}</div>}
       </section>
       <section className='flex gap-x-2 justify-between'>
-        <div className='flex flex-col gap-y-2'>
+        <div className='flex flex-col justify-around'>
           <span className='text-xl'>{blurb}</span>
           {children}
         </div>
@@ -26,7 +26,7 @@ function AssignmentCard({ title, blurb, image, tags, livelink, repolink, childre
       </section>
       <footer className='flex justify-between border-t py-2 mt-1'>
         {livelink ?
-          <a className='hover:bg-white/10 text-white p-1 md:text-lg hover:underline border-amber-100 border'
+          <a className='hover:bg-white/10 text-white p-1 md:text-lg hover:underline border-orange-100 border'
             href={livelink}>Visit Live</a> : null}
         <a className='hover:bg-white/10 text-white p-1 md:text-lg hover:underline border-blue-100 border'
           href={repolink}>View Source</a>
@@ -46,7 +46,14 @@ function App() {
          Written in TypeScript and React, built with Vite.' image='/react-circles-screenshot.webp'
           tags={['React', 'TypeScript']} livelink='https://circles.frontend.nextdev.in/'
           repolink='https://github.com/furquan-lp/frontend-interview/tree/master/react-circles'>
-          <span>???</span>
+          <ul className='list-disc list-inside md:text-lg'>
+            <div className='font-bold md:text-xl mb-2'>Problem Statement:</div>
+            <li>Make a basic single page app where the user can click anywhere on the page to place a circle on that
+              location</li>
+            <li>Add two buttons—undo and redo—to undo and redo these placements</li>
+            <li>Add a reset button to clear the entire page of these circles
+            </li>
+          </ul>
         </AssignmentCard>
       </section>
     </main>
