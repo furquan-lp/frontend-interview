34df46594b21b18495414950b8a6e3cada527a0f
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 14:04:16 +0530

    Added a key prop for the select options being mapped

--- sql-editor/src/app/components/viewtable.tsx
+++ sql-editor/src/app/components/viewtable.tsx
@@ -4,7 +4,7 @@ export default function ViewTable({ tables }: { tables: string[] }) {
       <span>Spectating Table:</span>
       <select className='p-1 px-2 rounded-md bg-slate-200 dark:bg-slate-600'>
         <option value='none'>Select Table</option>
-        {tables.length && tables.map(t => <option value={`${t}`}>{t}</option>)}
+        {tables.length && tables.map((t, i) => <option value={`${t}`} key={t + ViewTable + i}>{t}</option>)}
       </select>
       <button className='material-symbols-outlined text-slate-600 hover:bg-slate-100 dark:text-slate-100
        dark:hover:bg-slate-600 p-1 rounded-full'>Sync</button>
