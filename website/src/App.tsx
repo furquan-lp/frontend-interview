befe152dc3b39fbb387d874d8ff3383875abcd42
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 23:29:47 +0530

    Adjusted the width of images and the mid section in AssignmentCard and added the card for SQL Editor

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -12,17 +12,18 @@ function AssignmentCard({ title, blurb, image, tags, livelink, repolink, childre
   tags?: string[], livelink?: string, repolink: string, children?: JSX.Element
 }) {
   return (
-    <article className='flex flex-col gap-y-2 border rounded border-white p-2 text-white my-4 md:my-10 w-full bg-white/5'>
+    <article className='flex flex-col gap-y-2 border rounded border-white p-2 text-white my-4 md:my-10 w-full
+     bg-white/5'>
       <section className='flex flex-col border-b'>
         <span className='text-3xl font-bold'>{title}</span>
         {tags && <div className='flex my-2'>{tags.map(t => <AssignmentCardTag tag={t} />)}</div>}
       </section>
       <section className='flex gap-x-2 justify-between'>
-        <div className='flex flex-col justify-around'>
+        <div className='flex flex-col justify-around md:w-2/3'>
           <span className='text-xl'>{blurb}</span>
           {children}
         </div>
-        <img src={image} className='w-2/3 rounded-md' />
+        <img src={image} className='flex md:w-1/2 grow rounded-md' />
       </section>
       <footer className='flex justify-between border-t py-2 mt-1'>
         {livelink ?
@@ -42,6 +43,20 @@ function App() {
       <section className='flex flex-col md:mt-10 w-2/3 mx-auto'>
         <span className='text-white text-2xl italic'>Assorted frontend projects, interview questions
           and assignments. </span>
+        <AssignmentCard title='SQL Editor' blurb='A backendless responsive SQL Editor web app built with Next.js and
+         TypeScript. Runs SQLite in the browser using WebAssembly and requires nothing on the backend.'
+          image='/sql-editor-screenshot.webp' tags={['Next.js', 'TypeScript', 'SQLite', 'WASM']}
+          livelink='https://sql.frontend.nextdev.in/'
+          repolink='https://github.com/furquan-lp/frontend-interview/tree/master/sql-editor'>
+          <ul className='list-disc list-inside md:text-lg'>
+            <div className='font-bold md:text-xl mb-2'>Features:</div>
+            <li>Execute SQL code locally without requiring an external database or a file and display the output of
+              queries being run</li>
+            <li>Live viewing of all the tables in the database</li>
+            <li>Seamless toggle for dark mode theming while respecting system choice</li>
+            <li>Run on a mobile screen or smaller sized viewports (responsive design)</li>
+          </ul>
+        </AssignmentCard>
         <AssignmentCard title='React Circles' blurb='This is a frontend React interview assignment (junior level).
          Written in TypeScript and React, built with Vite.' image='/react-circles-screenshot.webp'
           tags={['React', 'TypeScript']} livelink='https://circles.frontend.nextdev.in/'
