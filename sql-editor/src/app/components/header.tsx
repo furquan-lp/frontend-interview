ddd0abedb663a94f29d95a61e2379aac16b88bc6
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 17:48:55 +0530

    Change the colors to be darker on dark mode

--- sql-editor/src/app/components/header.tsx
+++ sql-editor/src/app/components/header.tsx
@@ -4,18 +4,18 @@ export default function Header({ version, clickRun, setDarkMode, theme }: {
   version?: number, clickRun: MouseEventHandler<HTMLElement>, setDarkMode: Function, theme: string
 }) {
   return (
-    <header className='flex items-center justify-between bg-emerald-700 dark:bg-sky-700 p-2 text-white'>
+    <header className='flex items-center justify-between bg-emerald-700 dark:bg-sky-800 p-2 text-white'>
       <span className='flex gap-1 items-center'>
         <span className='text-2xl font-logofont select-none'>SQL Editor {version}</span>
-        <button className='rounded bg-green-600 active:bg-green-500 dark:bg-cyan-600 dark:active:bg-cyan-500
+        <button className='rounded bg-green-600 active:bg-green-500 dark:bg-cyan-700 dark:active:bg-cyan-600
          text-lg font-bold ml-6 px-2 p-1'><span className='hidden md:inline' onClick={clickRun}>Run</span> <span
             className='material-symbols-outlined align-[-6px]'>arrow_forward</span></button>
-        <button className='rounded bg-emerald-600 active:bg-emerald-500 dark:bg-sky-600 dark:active:bg-sky-500
+        <button className='rounded bg-emerald-600 active:bg-emerald-500 dark:bg-sky-700 dark:active:bg-sky-600
          text-lg font-bold px-2 p-1'><span className='hidden md:inline'>Clear</span> <span
             className='material-symbols-outlined align-[-6px]'>clear_all</span></button>
       </span>
       <span className='flex gap-1 items-center'>
-        <button className='material-symbols-outlined hover:bg-emerald-600 dark:hover:bg-sky-600 p-1 rounded-full'
+        <button className='material-symbols-outlined hover:bg-emerald-600 dark:hover:bg-sky-700 p-1 rounded-full'
           onClick={() => setDarkMode(theme === 'dark' ? 'light' : 'dark')}>
           {theme === 'dark' ? <>light_mode</> : <>dark_mode</>}</button>
         <a href='https://github.com/furquan-lp/frontend-interview/tree/master/sql-editor'
