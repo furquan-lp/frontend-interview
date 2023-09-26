762528282dd5b69fd021faa47384ddb09b7b6d6b
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 22:51:34 +0530

    In AssignmentCard: changed description to blurb prop, added middle section, and added children prop, changed the welcome text and passed the props for the react-circles AssignmentCard

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -7,13 +7,24 @@ function AssignmentCardTag({ tag }: { tag: string }) {
   );
 }
 
-function AssignmentCard({ title, description, tags }: { title: string, description: string, tags?: string[] }) {
+function AssignmentCard({ title, blurb, image, tags, children }: {
+  title: string, blurb: string, image: string,
+  tags?: string[], children?: JSX.Element
+}) {
   return (
-    <article className='border rounded border-white p-2 text-white my-4 md:my-10 w-full bg-white/5'>
+    <article className='flex flex-col gap-y-2 border rounded border-white p-2 text-white my-4 md:my-10 w-full bg-white/5'>
       <section className='flex flex-col border-b'>
         <span className='text-3xl font-bold'>{title}</span>
         {tags && <div className='flex my-2'>{tags.map(t => <AssignmentCardTag tag={t} />)}</div>}
       </section>
+      <section className='flex gap-x-2 justify-between'>
+        <div className='flex flex-col gap-y-2'>
+          <span className='text-xl'>{blurb}</span>
+          {children}
+        </div>
+        <img src={image} className='w-2/3 rounded-md' />
+      </section>
+      <span></span>
     </article>
   );
 }
@@ -22,9 +33,13 @@ function App() {
   return (
     <main>
       <Header />
-      <section className='flex flex-col items-center md:mt-10 w-1/2 mx-auto'>
-        <span className='text-white text-2xl italic'>Welcome to my frontend portfolio. </span>
-        <AssignmentCard title='React Circles' description='foo bar' tags={['React', 'TypeScript']} />
+      <section className='flex flex-col md:mt-10 w-2/3 mx-auto'>
+        <span className='text-white text-2xl italic'>Assorted frontend projects, interview questions
+          and assignments. </span>
+        <AssignmentCard title='React Circles' blurb='This is a frontend React interview assignment (junior level).
+         Written in TypeScript and React, built with Vite.' image='/react-circles-screenshot.webp' tags={['React', 'TypeScript']}>
+          <span>???</span>
+        </AssignmentCard>
       </section>
     </main>
   );
