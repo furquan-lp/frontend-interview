b3c22393428c053987b0f4e8a3e027f837a7d014
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 20:14:56 +0530

    Added overflow-y to SQLOutputField

--- sql-editor/src/app/components/outputfield.tsx
+++ sql-editor/src/app/components/outputfield.tsx
@@ -2,8 +2,10 @@ export default function SQLOutputField({ messages }: { messages?: string[] }) {
   return (
     <div className='flex flex-col border border-slate-300 bg-slate-500 text-white dark:bg-slate-100
      dark:border-slate-900 dark:text-slate-700 w-full h-64 md:h-96 p-2 py-5 rounded-md shadow focus:outline-none
-      resize-none font-mono md:text-lg'>{messages ? messages.map((m, i) => <span className={m.includes('Error') ?
-      'text-red-400 dark:text-orange-500' : undefined} key={i + m}>{m}</span>
+      resize-none font-mono md:text-lg overflow-y-auto'>{messages ? messages.map((m, i) =>
+      <span className={m.includes('Error') ? 'text-red-400 dark:text-orange-500' : undefined} key={i + m}>
+        {m}
+      </span>
     ) : 'Loading...'}
     </div>
   );
