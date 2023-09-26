952e56155d260fc8e7d8657b378794a93af7f85e
Syed F Ahmad <furquan@protonmail.com>
2023-09-26 17:47:47 +0530

    Set a dark theme border for the textarea

--- sql-editor/src/app/components/sqlfield.tsx
+++ sql-editor/src/app/components/sqlfield.tsx
@@ -1,13 +1,13 @@
-import { ChangeEventHandler, MutableRefObject } from "react";
+import { ChangeEventHandler } from "react";
 
 export default function SQLField({ onChange, loaded }: {
   onChange: ChangeEventHandler<HTMLTextAreaElement>,
   loaded: boolean
 }) {
   return (
-    <textarea className='border border-slate-300 bg-slate-50 bg-clip-content w-full h-64 md:h-96 p-1 py-2 md:p-2
-     md:py-5 rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600 dark:text-slate-50
-      font-editorfont md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange} disabled={!loaded}
-      id='sqltextarea' />
+    <textarea className='border border-slate-300 dark:border-slate-500 bg-slate-50 bg-clip-content w-full h-64
+     md:h-96 p-1 py-2 md:p-2 md:py-5 rounded-md shadow focus:outline-none dark:bg-clip-padding dark:bg-slate-600
+      dark:text-slate-50 font-editorfont md:text-xl' placeholder='SELECT * FROM sqlite_master;' onChange={onChange}
+      disabled={!loaded} id='sqltextarea' />
   );
 }
