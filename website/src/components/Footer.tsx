702f486389baef51114b7740dc633453503743c5
Syed F Ahmad <furquan@protonmail.com>
2023-09-27 22:56:27 +0530

    Set the margin/width on the quote for md+ screens only and increased its font for md+ screens

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className='flex flex-col mt-2 md:mt-6 text-white'>
-      <i className='w-2/3 mx-auto my-2 md:my-6'>"If debugging is the process of removing software bugs, then
-        programming must be the process of putting them in." - Edsger Dijkstra.</i>
+      <i className='mx-1 md:w-2/3 md:mx-auto my-2 md:my-6 md:text-lg'>"If debugging is the process of removing software
+        bugs, then programming must be the process of putting them in." - Edsger Dijkstra.</i>
       <span className='p-1 bg-white/20 justify-between items-center'>Copyright &copy; 2023 Syed Furquan Ahmad.</span>
     </footer>
   );
