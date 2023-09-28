3cf975da00fa251aaf435b064ad1cfcf346856ac
Syed F Ahmad <furquan@protonmail.com>
2023-09-28 23:45:42 +0530

    Added a Footer component with Copyright notice

---
+++ phone-store/src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='flex items-center p-2 bg-cyan-800 text-white justify-between m-2 rounded-lg'>
+      Copyright &copy; 2023 Syed Furquan Ahmad.
+    </footer>
+  );
+}
