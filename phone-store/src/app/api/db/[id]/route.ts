958d7f56f429bdb24babb814c4a1a87355f202f4
Syed F Ahmad <furquan@protonmail.com>
2023-10-08 18:04:02 +0530

    Removed logging statement

--- phone-store/src/app/api/db/[id]/route.ts
+++ phone-store/src/app/api/db/[id]/route.ts
@@ -3,7 +3,6 @@ import conn from '@/app/lib/db';
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
     const phoneQuery = await conn.query(`SELECT * FROM phone_models WHERE id = '${params.id}'`);
-    console.log(phoneQuery.rows);
     if (phoneQuery.rows.length === 0) {
       return new Response(`Unknown id: '${params.id}'`, { status: 404 });
     } else {
