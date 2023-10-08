e39f20e2691387c001d9d2211fae207656a2ff18
Syed F Ahmad <furquan@protonmail.com>
2023-10-08 20:50:20 +0530

    Added a header for the PhonePage page

---
+++ phone-store/src/app/phones/[id]/components/header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className='flex items-center p-2 bg-cyan-800 justify-between m-2 rounded-lg'>
+      <span className='font-source-code-pro italic text-2xl text-white select-none'>phone_store</span>
+      <span className='flex items-center'>
+        <Link href='/about' className='text-white p-1 border rounded-md rounded-r-none'>About</Link>
+        <a href='https://github.com/furquan-lp/frontend-interview/tree/master/phone-store'
+          className='text-white p-1 border border-l-transparent rounded-l-none rounded-md'>
+          <span className='p-0.5' data-feather='github' />
+        </a>
+      </span>
+    </header>
+  );
+}
