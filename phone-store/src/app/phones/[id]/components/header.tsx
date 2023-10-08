e9ec758896ddf7f6601af3ad83bf40294008fd7b
Syed F Ahmad <furquan@protonmail.com>
2023-10-08 21:23:36 +0530

    Added back button to header

--- phone-store/src/app/phones/[id]/components/header.tsx
+++ phone-store/src/app/phones/[id]/components/header.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className='flex items-center p-2 bg-cyan-800 justify-between m-2 rounded-lg'>
+      <Link href='/' className='p-1 rounded-full text-white text-4xl hover:bg-cyan-700'>
+        <span className='p-0.5' data-feather='arrow-left-circle' ></span>
+      </Link>
       <span className='font-source-code-pro italic text-2xl text-white select-none'>phone_store</span>
       <span className='flex items-center'>
         <Link href='/about' className='text-white p-1 border rounded-md rounded-r-none'>About</Link>
