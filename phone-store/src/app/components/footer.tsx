c71e1570d273e18b70ddd43e92520e4b912496c3
Syed F Ahmad <furquan@protonmail.com>
2023-10-08 21:46:30 +0530

    Made the footer sticky

--- phone-store/src/app/components/footer.tsx
+++ phone-store/src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='flex items-center p-2 bg-cyan-800 text-white justify-between m-2 rounded-lg'>
+    <footer className='flex items-center p-2 bg-cyan-800 text-white justify-between m-2 rounded-lg sticky top-[100vh]'>
       Copyright &copy; 2023 Syed Furquan Ahmad. Powered by Amazon RDS.
       <span>
         Images provided by <a className="hover:underline text-orange-300" href='https://vecteezy.com/'>Vecteezy</a>
